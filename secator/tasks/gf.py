608ca96df190991b857a12757ee7091c80c7c0e0
itsbrex <roach7@gmail.com>
2025-02-02 00:28:45 -0800

    Refactor gf.py for improved code structure and readability

--- secator/tasks/gf.py
+++ secator/tasks/gf.py
@@ -6,29 +6,26 @@ from secator.tasks._categories import Tagger
 
 @task()
 class gf(Tagger):
-	"""Wrapper around grep, to help you grep for things."""
-	cmd = 'gf'
-	file_flag = OPT_PIPE_INPUT
-	input_flag = OPT_PIPE_INPUT
-	version_flag = OPT_NOT_SUPPORTED
-	opts = {
-		'pattern': {'type': str, 'help': 'Pattern names to match against (comma-delimited)'}
-	}
-	opt_key_map = {
-		'pattern': ''
-	}
-	input_type = URL
-	install_cmd = (
-		'go install -v github.com/tomnomnom/gf@latest && '
-		'git clone https://github.com/1ndianl33t/Gf-Patterns $HOME/.gf || true'
-	)
-	output_types = [Tag]
+    """Wrapper around grep, to help you grep for things."""
 
-	@staticmethod
-	def item_loader(self, line):
-		return {'match': line, 'name': self.get_opt_value('pattern').rstrip() + ' pattern'}  # noqa: E731,E501
+    cmd = "gf"
+    file_flag = OPT_PIPE_INPUT
+    input_flag = OPT_PIPE_INPUT
+    version_flag = OPT_NOT_SUPPORTED
+    opts = {"pattern": {"type": str, "help": "Pattern names to match against (comma-delimited)"}}
+    opt_key_map = {"pattern": ""}
+    input_type = URL
+    install_cmd = (
+        "go install -v github.com/tomnomnom/gf@latest && "
+        "git clone https://github.com/1ndianl33t/Gf-Patterns $HOME/.gf || true"
+    )
+    output_types = [Tag]
 
-	@staticmethod
-	def on_item(self, item):
-		item.extra_data = {'source': 'url'}
-		return item
+    @staticmethod
+    def item_loader(self, line):
+        return {"match": line, "name": self.get_opt_value("pattern").rstrip() + " pattern"}  # noqa: E731,E501
+
+    @staticmethod
+    def on_item(self, item):
+        item.extra_data = {"source": "url"}
+        return item
