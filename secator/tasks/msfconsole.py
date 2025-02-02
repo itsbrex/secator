6a4a66e8d5d71ba713cd809b19e1419fd2054569
itsbrex <roach7@gmail.com>
2025-02-02 00:29:18 -0800

    Refactor msfconsole.py for improved code structure and readability

--- secator/tasks/msfconsole.py
+++ secator/tasks/msfconsole.py
@@ -5,8 +5,19 @@ import logging
 from rich.panel import Panel
 
 from secator.decorators import task
-from secator.definitions import (DELAY, FOLLOW_REDIRECT, HEADER, HOST, OPT_NOT_SUPPORTED, PROXY, RATE_LIMIT, RETRIES,
-								 THREADS, TIMEOUT, USER_AGENT)
+from secator.definitions import (
+    DELAY,
+    FOLLOW_REDIRECT,
+    HEADER,
+    HOST,
+    OPT_NOT_SUPPORTED,
+    PROXY,
+    RATE_LIMIT,
+    RETRIES,
+    THREADS,
+    TIMEOUT,
+    USER_AGENT,
+)
 from secator.tasks._categories import VulnMulti
 from secator.utils import get_file_timestamp
 
@@ -15,94 +26,90 @@ logger = logging.getLogger(__name__)
 
 @task()
 class msfconsole(VulnMulti):
-	"""CLI to access and work with the Metasploit Framework."""
-	cmd = 'msfconsole --quiet'
-	version_flag = OPT_NOT_SUPPORTED
-	input_type = HOST
-	input_chunk_size = 1
-	output_types = []
-	output_return_type = str
-	opt_prefix = '--'
-	opts = {
-		'resource': {'type': str, 'help': 'Metasploit resource script.', 'short': 'r'},
-		'execute_command': {'type': str, 'help': 'Metasploit command.', 'short': 'x'},
-		'environment': {'type': str, 'help': 'Environment variables string KEY=VALUE.', 'short': 'e'}
-	}
-	opt_key_map = {
-		'x': 'execute_command',
-		'r': 'resource',
-		HEADER: OPT_NOT_SUPPORTED,
-		DELAY: OPT_NOT_SUPPORTED,
-		FOLLOW_REDIRECT: OPT_NOT_SUPPORTED,
-		PROXY: OPT_NOT_SUPPORTED,
-		RATE_LIMIT: OPT_NOT_SUPPORTED,
-		RETRIES: OPT_NOT_SUPPORTED,
-		THREADS: OPT_NOT_SUPPORTED,
-		TIMEOUT: OPT_NOT_SUPPORTED,
-		USER_AGENT: OPT_NOT_SUPPORTED,
-		THREADS: OPT_NOT_SUPPORTED,
-	}
-	encoding = 'ansi'
-	ignore_return_code = True
-	# install_cmd = 'wget -O - https://raw.githubusercontent.com/freelabz/secator/main/scripts/msfinstall.sh | sh'
+    """CLI to access and work with the Metasploit Framework."""
 
-	@staticmethod
-	def validate_input(self, input):
-		"""No list input supported for this command. Pass a single input instead."""
-		if isinstance(input, list):
-			return False
-		return True
+    cmd = "msfconsole --quiet"
+    version_flag = OPT_NOT_SUPPORTED
+    input_type = HOST
+    input_chunk_size = 1
+    output_types = []
+    output_return_type = str
+    opt_prefix = "--"
+    opts = {
+        "resource": {"type": str, "help": "Metasploit resource script.", "short": "r"},
+        "execute_command": {"type": str, "help": "Metasploit command.", "short": "x"},
+        "environment": {"type": str, "help": "Environment variables string KEY=VALUE.", "short": "e"},
+    }
+    opt_key_map = {
+        "x": "execute_command",
+        "r": "resource",
+        HEADER: OPT_NOT_SUPPORTED,
+        DELAY: OPT_NOT_SUPPORTED,
+        FOLLOW_REDIRECT: OPT_NOT_SUPPORTED,
+        PROXY: OPT_NOT_SUPPORTED,
+        RATE_LIMIT: OPT_NOT_SUPPORTED,
+        RETRIES: OPT_NOT_SUPPORTED,
+        THREADS: OPT_NOT_SUPPORTED,
+        TIMEOUT: OPT_NOT_SUPPORTED,
+        USER_AGENT: OPT_NOT_SUPPORTED,
+        THREADS: OPT_NOT_SUPPORTED,
+    }
+    encoding = "ansi"
+    ignore_return_code = True
+    # install_cmd = 'wget -O - https://raw.githubusercontent.com/freelabz/secator/main/scripts/msfinstall.sh | sh'
 
-	@staticmethod
-	def on_init(self):
-		command = self.get_opt_value('execute_command')
-		script_path = self.get_opt_value('resource')
-		environment = self.run_opts.pop('environment', '')
-		env_vars = {}
-		if environment:
-			env_vars = dict(map(lambda x: x.split('='), environment.strip().split(',')))
-		env_vars['RHOST'] = self.input
-		env_vars['RHOSTS'] = self.input
+    @staticmethod
+    def validate_input(self, input):
+        """No list input supported for this command. Pass a single input instead."""
+        if isinstance(input, list):
+            return False
+        return True
 
-		# Passing msfconsole command directly, simply add RHOST / RHOSTS from host input and run then exit
-		if command:
-			self.run_opts['msfconsole.execute_command'] = (
-				f'setg RHOST {self.input}; '
-				f'setg RHOSTS {self.input}; '
-				f'{command.format(**env_vars)}; '
-				f'exit;'
-			)
+    @staticmethod
+    def on_init(self):
+        command = self.get_opt_value("execute_command")
+        script_path = self.get_opt_value("resource")
+        environment = self.run_opts.pop("environment", "")
+        env_vars = {}
+        if environment:
+            env_vars = dict(map(lambda x: x.split("="), environment.strip().split(",")))
+        env_vars["RHOST"] = self.input
+        env_vars["RHOSTS"] = self.input
 
-		# Passing resource script, replace vars inside by our environment variables if any, write to a temp file, and
-		# pass this temp file instead of the original one.
-		elif script_path:
+        # Passing msfconsole command directly, simply add RHOST / RHOSTS from host input and run then exit
+        if command:
+            self.run_opts["msfconsole.execute_command"] = (
+                f"setg RHOST {self.input}; " f"setg RHOSTS {self.input}; " f"{command.format(**env_vars)}; " f"exit;"
+            )
 
-			# Read from original resource script
-			with open(script_path, 'r') as f:
-				content = f.read().replace('exit', '') + 'exit'
+        # Passing resource script, replace vars inside by our environment variables if any, write to a temp file, and
+        # pass this temp file instead of the original one.
+        elif script_path:
 
-			# Make a copy and replace vars inside by env vars passed on the CLI
-			timestr = get_file_timestamp()
-			out_path = f'{self.reports_folder}/.inputs/msfconsole_{timestr}.rc'
-			logger.debug(
-				f'Writing formatted resource script to new temp file {out_path}'
-			)
-			with open(out_path, 'w') as f:
-				content = content.format(**env_vars)
-				f.write(content)
+            # Read from original resource script
+            with open(script_path, "r") as f:
+                content = f.read().replace("exit", "") + "exit"
 
-			script_name = script_path.split('/')[-1]
-			self._print(Panel(content, title=f'[bold magenta]{script_name}', expand=False))
+            # Make a copy and replace vars inside by env vars passed on the CLI
+            timestr = get_file_timestamp()
+            out_path = f"{self.reports_folder}/.inputs/msfconsole_{timestr}.rc"
+            logger.debug(f"Writing formatted resource script to new temp file {out_path}")
+            with open(out_path, "w") as f:
+                content = content.format(**env_vars)
+                f.write(content)
 
-			# Override original command with new resource script
-			self.run_opts['msfconsole.resource'] = out_path
+            script_name = script_path.split("/")[-1]
+            self._print(Panel(content, title=f"[bold magenta]{script_name}", expand=False))
 
-		# Nothing passed, error out
-		else:
-			raise ValueError('At least one of "inline_script" or "resource_script" must be passed.')
+            # Override original command with new resource script
+            self.run_opts["msfconsole.resource"] = out_path
 
-		# Clear host input
-		self.input = ''
+        # Nothing passed, error out
+        else:
+            raise ValueError('At least one of "inline_script" or "resource_script" must be passed.')
+
+        # Clear host input
+        self.input = ""
 
 
 # TODO: This is better as it goes through an RPC API to communicate with
